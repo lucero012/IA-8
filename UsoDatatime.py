2fa420e16ae61d14057a728b3f0e40b9ba031fc4
Lucero <90294297+lucero012@users.noreply.github.com>
2025-02-12 20:45:01 -0600

    Ejercicio ejemplo

---
+++ UsoDatatime.py
@@ -0,0 +1,4 @@
+import datetime
+nombre= input ("Ingresa tu nombre: ")
+fecha =datetime.datetime.now().strftime("%Y-%m_%d %H:%M:%S")
+print(f"Nombre del cliente: {nombre} y la fecha y hora: {fecha}")
